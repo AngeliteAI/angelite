23775905f6412ff763daddcb49899bad60a9827a
solmidnight <sol@blockglow.com>
2025-02-10 15:37:18 -0600

    missing?

--- unity/Prism/Assets/World.cs
+++ unity/Prism/Assets/World.cs
@@ -6,11 +6,12 @@ public class World : MonoBehaviour
 {
     public int cubeSize = 1; // Size of each cube
     public float freq = 1.0f;
+    public float scale = 0.5f;
     public Vector3Int worldSize = new Vector3Int(10, 1, 10); // Size of the world (number of cubes in each dimension)
     public Vector3Int chunkSize = new Vector3Int(8, 8, 8); // Size of the world (number of cubes in each dimension)
     const float GOLDEN = 0.618033988f;
             public float squishFactor = 0.6f; // Adjust this value to control the squish effect
-            public float heightOffset = 32f; // Adjust this value to control the overall height of the terrain
+            public float heightOffset = 8f; // Adjust this value to control the overall height of the terrain
     public int[,,] worldData; // 3D boolean array to store world data
        private void Start()
         {
@@ -19,31 +20,35 @@ public class World : MonoBehaviour
     
         private void GenerateWorld()
         {
-            GameObject worldObject = new GameObject("World");
-            worldObject.transform.parent = transform;
-
             // Initialize world data
             worldData = new int[worldSize.x * chunkSize.x, worldSize.y * chunkSize.y, worldSize.z * chunkSize.z];
 
-
+            var noiseData = Noise.Calc3D(worldSize.x * chunkSize.x, worldSize.y * chunkSize.y, worldSize.z * chunkSize.z, scale);
+            ;
             for (int z = 0; z < worldSize.z * chunkSize.z; z++)
             {
                 for (int x = 0; x < worldSize.x * chunkSize.x; x++)
                 {
                     float sampleX = (float)x / ((float)worldSize.x * (float)chunkSize.x) * GOLDEN * freq;
                     float sampleZ = (float)z / ((float)worldSize.z * (float)chunkSize.z) * GOLDEN * freq;
-                    Debug.Log(sampleX);
-                    float noise3D = Noise3D(sampleX, sampleZ, squishFactor);
-                    int height = Mathf.RoundToInt(noise3D * (float)worldSize.y * (float)chunkSize.y + heightOffset);
-                    Debug.Log(height);
-                    
-                    for (int y = 0; y < Math.Min(height, 7); y++)
+
+                    for (int y = 0; y < worldSize.y * chunkSize.y; y++)
                     {
-                        worldData[x, y, z] = 1;
+                        float sampleY = (float)y / ((float)worldSize.y * (float)chunkSize.y) * freq;
+                        float density = Noise.CalcPixel3D((int)sampleX, (int)sampleY, (int)sampleZ, scale) - heightOffset;
+                        float densityMod = squishFactor * (float)y;
+
+                        if (density + densityMod > 0)
+                        {
+                            worldData[x, y, z] = 1;
+                        }
+                        else
+                        {
+                            worldData[x, y, z] = 0;
+                        }
                     }
                 }
-            }
-
+            }     
             for (int z = 0; z < worldSize.z; z++)
             {
                 for (int x = 0; x < worldSize.x; x++)
