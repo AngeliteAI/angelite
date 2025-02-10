9129075aeee5cafd1d5c9f8c0bc2aefd2518b626
solmidnight <sol@blockglow.com>
2025-02-10 14:34:45 -0600

    Ugly terrain

--- unity/Prism/Assets/ObliqueMover.cs
+++ unity/Prism/Assets/ObliqueMover.cs
@@ -9,10 +9,10 @@ public class ObliqueMover : MonoBehaviour
         var frameExcess = new Vector3(this.transform.position.x % 1.0f, this.transform.position.y % 1.0f,
             this.transform.position.z % 1.0f);
         this.excess += frameExcess;
-        var temp = this.transform.position;
+        var temp = new Vector3(0f,0f,0f);
         for (int d = 0; d < 3; d++)
         {
-            if (Mathf.Abs(this.excess[d]) > 1.0)
+            if (Mathf.Abs(this.excess[d]) >= 1.0)
             {
                 temp[d] += this.excess[d];
                 this.excess[d] = 0.0f;
--- unity/Prism/Assets/World.cs
+++ unity/Prism/Assets/World.cs
@@ -8,6 +8,9 @@ public class World : MonoBehaviour
     public float freq = 1.0f;
     public Vector3Int worldSize = new Vector3Int(10, 1, 10); // Size of the world (number of cubes in each dimension)
     public Vector3Int chunkSize = new Vector3Int(8, 8, 8); // Size of the world (number of cubes in each dimension)
+    const float GOLDEN = 0.618033988f;
+            public float squishFactor = 0.6f; // Adjust this value to control the squish effect
+            public float heightOffset = 32f; // Adjust this value to control the overall height of the terrain
     public int[,,] worldData; // 3D boolean array to store world data
        private void Start()
         {
@@ -21,14 +24,22 @@ public class World : MonoBehaviour
 
             // Initialize world data
             worldData = new int[worldSize.x * chunkSize.x, worldSize.y * chunkSize.y, worldSize.z * chunkSize.z];
+
+
             for (int z = 0; z < worldSize.z * chunkSize.z; z++)
             {
                 for (int x = 0; x < worldSize.x * chunkSize.x; x++)
                 {
-                    var height = (int)(Mathf.PerlinNoise(x * freq, z * freq) * (float) worldSize.y * (float) chunkSize.y);
-                    for (int y = 0; y < height; y++)
+                    float sampleX = (float)x / ((float)worldSize.x * (float)chunkSize.x) * GOLDEN * freq;
+                    float sampleZ = (float)z / ((float)worldSize.z * (float)chunkSize.z) * GOLDEN * freq;
+                    Debug.Log(sampleX);
+                    float noise3D = Noise3D(sampleX, sampleZ, squishFactor);
+                    int height = Mathf.RoundToInt(noise3D * (float)worldSize.y * (float)chunkSize.y + heightOffset);
+                    Debug.Log(height);
+                    
+                    for (int y = 0; y < Math.Min(height, 7); y++)
                     {
-                        worldData[x, y, z] = 1;     
+                        worldData[x, y, z] = 1;
                     }
                 }
             }
@@ -43,8 +54,18 @@ public class World : MonoBehaviour
                     chunkGameObject.AddComponent<Chunk>();
                     var chunk = chunkGameObject.GetComponent<Chunk>();
                     chunk.world = this;
-                    chunk.chunkPosition =  new Vector3Int(x, 0, z);
+                    chunk.chunkPosition = new Vector3Int(x, 0, z);
                 }
             }
+        }
+
+        private float Noise3D(float x, float y, float squishFactor)
+        {
+            // Calculate the 3D noise value using Perlin noise
+            float noiseX = x;
+            float noiseY = y * squishFactor;
+            float noiseZ = y * (1 - squishFactor);
+
+            return Mathf.Max(0.0f, Mathf.PerlinNoise(noiseX, noiseY) + Mathf.PerlinNoise(noiseX, noiseZ));
         }
 }
