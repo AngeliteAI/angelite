e132d87900c49578ee22b5e828c4eef797f1e86f
solmidnight <sol@angelite.ai>
2025-03-08 21:19:24 +0000

    Work on website

--- src/javascript/angelite-ai/src/lib/Sidebar.svelte
+++ src/javascript/angelite-ai/src/lib/Sidebar.svelte
@@ -1,5 +1,104 @@
-<div id="sidebar" class="absolute top-0 left-0 z-50 h-full w-[50px]">
-    <img src="/icon.png" class="h-[50px]"/>
-    <span class="flex-grow"/>
-    <img src="/sidebar.png"/>
+<script>
+    let isTransformed = $state(false);
+    let sidebarOpened = $state(false);
+
+    function mouseover() {
+        isTransformed = true;
+    }
+
+    function mouseleave() {
+        isTransformed = false;
+    }
+
+    function open() {
+        sidebarOpened = true;
+    }
+
+    function close() {
+        sidebarOpened = false;
+    }
+</script>
+
+<span id="sidebar-container" class="absolute top-0 h-full w-full">
+
+<div id="sidebar" class="cursor-pointer select-none z-100 absolute flex flex-col top-0 left-0 h-full w-[50px]" on:mouseover={() => open()} 
+  on:mouseleave={() => close()}>
+
+    <span class="icon">
+    <img src="/icon.png" class=" h-[40px]  "/>
+    </span>
+    <span class="flex-grow "/>
+    <span class="icon">
+    <img src="/sidebar.png" class="sidebar-icon invert h-[30px]"/>
+    </span>
 </div>
+
+<span id="menu-container" class="absolute" class:changed={sidebarOpened}>
+<span id="menu" class="absolute">
+</span>
+</span>
+
+<span 
+  id="trigger" 
+  class="absolute top-0 z-50 cursor-grab h-full w-[200px]" 
+  on:mouseover={() => mouseover()} 
+  on:mouseleave={() => mouseleave()}>
+</span>
+
+<span id="hint-container" class:moved={sidebarOpened}>
+<span id="hint"  class:transformed={!isTransformed}></span>
+
+</span>
+</span>
+
+<style>
+    .icon {
+        @apply flex justify-center items-center h-[50px] cursor-pointer z-100;
+    }
+
+    #hint {
+        @apply absolute z-0 pointer-events-none w-[200px] h-[100vh];
+        top: 0;
+        left: 0;
+        bottom: 0;
+        background-image: linear-gradient(to right, var(--color-accent), transparent);
+        transition: left 0.3s  cubic-bezier(0.4, 0, 1, 1), opacity 0.2s cubic-bezier(0.4, 0, 1, 1);
+        opacity: 0.25;
+    }
+
+#hint-container {
+        @apply  relative z-0 pointer-events-none w-[200px] h-[100vh];
+        top: 0;
+        left: 0;
+        bottom: 0;
+        transition: left 0.3s  cubic-bezier(0.4, 0, 1, 1), opacity 0.2s cubic-bezier(0.4, 0, 1, 1);
+        opacity: 1.0;
+}
+
+.moved {
+    left:200px !important;
+    opacity: 0 !important;
+}
+    
+    .transformed {
+        left: -200px !important;
+    }
+
+#menu-container {
+    @apply w-[200px] h-full;
+    top: 0;
+    bottom: 0;
+    border-right: 1px var(--color-accent) solid;
+    transition: left 0.3s  cubic-bezier(0.4, 0, 1, 1), right 0.2s cubic-bezier(0.4, 0, 1, 1);
+    left: -200px;
+    right: 0;
+}
+
+#menu-container.changed {
+    left: 0px;
+    right: 200px; 
+}
+
+
+
+</style>
--- src/javascript/angelite-ai/src/routes/+page.svelte
+++ src/javascript/angelite-ai/src/routes/+page.svelte
@@ -3,15 +3,19 @@
 </script>
 
 <main class="w-full h-full flex justify-center">
-    <div id="container" class="color-text max-w-[800px] w-full h-[400px]">
+    <div id="container" class="color-text max-w-[800px] w-full h-[560px]">
         <div id="hero" class="flex flex-col justify-end items-start h-full">
+            <span class="select-none">
             <h1 class="text-6xl">Connection, simplified.</h1>
             <h2 class="text-3xl text-secondary">Universal language bindings, AI powered.</h2>
+            </span>
             <br/>
             <br/>
+            <span class="select-none">
             <Button><h1 class="text-2xl">Get Started</h1></Button>
             <br/>
             <span class="accent-button"><Button><h1 class="text-2xl">Pricing</h1></Button></span>
+            </span>
         </div>
     </div>
 </main>
