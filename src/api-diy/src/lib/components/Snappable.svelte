90bb3542a6857dad53231846c45ae0d471a98813
solmidnight <sol@blockglow.com>
2025-05-18 14:56:06 -0500

    Work on snapping

--- src/api-diy/src/lib/components/Snappable.svelte
+++ src/api-diy/src/lib/components/Snappable.svelte
@@ -7,13 +7,30 @@
      let clientHeight = $state();
 
     function triggerSnap(e) {
+        // Check if we should only allow dragging from specific handle
+        var element = e.currentTarget;
+        const treeWalker = document.createTreeWalker(element, NodeFilter.SHOW_ELEMENT);
+        let isParent = false;
+        while (treeWalker.nextNode()) {
+            if(treeWalker.currentNode == document.elementFromPoint(e.clientX, e.clientY))
+        {
+            break;
+        }
+            if (treeWalker.currentNode.classList.contains("snappable")) {
+                isParent = true;
+                break;
+            }
+        }
+        if (isParent) {
+            return;
+        }
         console.log(clientWidth, clientHeight);
     }
 </script>
 
 <!-- Just a basic wrapper div that passes all the content through -->
 <Draggable>
-    <div bind:clientWidth bind:clientHeight onclick={triggerSnap}>
+    <div bind:clientWidth bind:clientHeight class="snappable" onclick={triggerSnap}>
         {@render children()}
     </div>
 </Draggable>
